35b8d74b0a541fcdf601d84cac1b91ad0eb14404
Sophie Cap <sophie.capricorne@gmail.com>
2019-12-29 10:31:47 +0100

    modified ViewHolderAdapter class

--- app/src/main/java/com/sophie/magiworldandroid/RecyclerViewAdapter.java
+++ app/src/main/java/com/sophie/magiworldandroid/RecyclerViewAdapter.java
@@ -15,7 +15,7 @@ import java.util.List;
 /**
  * Created by SOPHIE on 22/12/2019.
  */
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.CustomViewHolder> {
     private List<String> mNumberOfTurn;
     private List<String> mAttackDamage;
 
@@ -26,16 +26,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerViewAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.history_recyclerview, parent, false);
-        return new ViewHolder(view);
+        return new CustomViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ((ViewHolder)holder).numberOfTurn.setText(mNumberOfTurn.get(position));
-        ((ViewHolder)holder).attackDamages.setText(mAttackDamage.get(position));
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.CustomViewHolder holder, int position) {
+       holder.bind(mNumberOfTurn.get(position),mAttackDamage.get(position));
     }
 
     @Override
@@ -44,14 +43,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
         TextView numberOfTurn;
         TextView attackDamages;
 
-        public ViewHolder (View itemView){
+        public CustomViewHolder (View itemView){
             super(itemView);
             numberOfTurn = itemView.findViewById(R.id.number_of_turn);
             attackDamages = itemView.findViewById(R.id.attack_history);
+        }
+        public void bind(String numberOfTurn, String attackDamages){
+            this.numberOfTurn.setText(numberOfTurn);
+            this.attackDamages.setText(attackDamages);
         }
     }
 }
